80daee326eb223d6688f7fd3cc6bdbf3d2c15ac6
Михаил <shonovgacha@gmail.com>
2025-04-23 15:05:49 +0300

    add fav page

--- src/components/Fav/Fav.jsx
+++ src/components/Fav/Fav.jsx
@@ -1,15 +1,47 @@
+import { useEffect, useState } from 'react'
 import styles from './Fav.module.scss'
 import { useSelector } from 'react-redux'
 
+import CharactersList from '../CharactersList/CharactersList'
+
 export default function Fav() {
+    const [people, setPeople] = useState([])
+
     const storeData = useSelector((state) => state.favReducer)
 
-    console.log(storeData)
+    useEffect(() => {
+        const arr = Object.entries(storeData)
+
+        if (arr.length) {
+            const res = arr.map((item) => {
+                return {
+                    url: item[0],
+                    ...item[1],
+                }
+            })
+
+            setPeople(res)
+        }
+    }, [])
 
     return (
         <>
-            <h1>Fav Page</h1>
-            {/* <p>{storeData}</p> */}
+            <div className="main__container">
+                {people.length ? (
+                    <CharactersList people={people} />
+                ) : (
+                    <h2
+                        style={{
+                            textAlign: 'center',
+                            marginTop: '50px',
+                            fontSize: '72px',
+                            fontWeight: '800',
+                        }}
+                    >
+                        No Data
+                    </h2>
+                )}
+            </div>
         </>
     )
 }
